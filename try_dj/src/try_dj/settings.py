3fe693fe5ae7dcce4a71334b3783a77467d64bb4
ANKIT JAILWAL <jailwalankit@gmail.com>
2020-11-13 23:41:49 +0530

    Updated

--- try_dj/src/try_dj/settings.py
+++ try_dj/src/try_dj/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': 'localhost',
+        'PORT': '8000',
     }
 }
 
